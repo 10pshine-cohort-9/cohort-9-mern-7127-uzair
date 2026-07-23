fb84733189820cee1876b70ed1ba16c9cc06c46c
Uzair5421 <itsuzair44@gmail.com>
2026-07-23 13:46:39 +0500

    Setup the backend project structure using Express and Mongoose

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const app = require('./src/app.js');
+const connectDB = require('./src/config/db.js');
+
+connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${PORT}`);
+});
