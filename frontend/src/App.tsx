7d8599590cd61d9903bbdafa2e46489727309f37
Uzair5421 <itsuzair44@gmail.com>
2026-07-24 23:54:42 +0500

    Fixed the CodeRabbit Error: The Typescript error having High vulnerability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter,Routes, Route, Navigate } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-import { useState } from 'react'
 
 
 function App() {
